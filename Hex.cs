31a6a5dc00c79f9d35cafb7bdfddc8f49b0d66dc
Paul <Klaseher@gmx.de>
2020-01-07 19:28:56 +0100

    Add files via upload
    Temporäre von mir(Paul) erstellte Hexklasse

---
+++ Hex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex : MonoBehaviour
+{
+    Boolean discovered=false;
+    Hex previous;
+
+    public Hex[] Neighbors; 
+    
+    public int xCoordinate { get; set; }
+    public int yCoordinate { get; set; }    
+
+    public Hex()
+    {
+
+    }
+
+    public void setDiscovered()
+    {
+        discovered = true;
+    }
+
+    public Boolean getDiscovered()
+    {
+        return discovered;
+    }
+
+    public Hex[] getNeighbors()
+    {
+        return Neighbors;
+    }    
+
+    public void setPrevious(Hex prev)
+    {
+        previous = prev; 
+    }
+
+    public Hex getPrevious(Hex prev)
+    {
+        return previous;
+    }
+}
