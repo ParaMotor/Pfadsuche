41c4dda153ef73e27ea333bef6421df729c0e5b0
MrBrownie221 <59098300+MrBrownie221@users.noreply.github.com>
2020-01-08 16:21:49 +0100

    Add files via upload

---
+++ Voreinstellungen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Voreinstellungen : MonoBehaviour
+{
+    //public GameObject inputField;
+    public int groesse;
+    //über start(int Wert) das Grid erstellen lassen, Wert ist dabei die Größe, wir wollen ein quadratisches Feld, dafür reicht eine Variable die Methode nur in die von Play einfügen
+    //start(Wert)
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GridGroesse(string eingabe)
+    {
+        groesse = int.Parse(eingabe);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
