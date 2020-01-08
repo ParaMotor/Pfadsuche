1499f37b45c2d213f443b67c372d7ff0b281f77a
Schub1 <45119707+Schub1@users.noreply.github.com>
2020-01-08 12:01:26 +0100

    Add files via upload

---
+++ StartMenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuButtons : MonoBehaviour
+{
+    //über start(int Wert) das Grid erstellen lassen, Wert ist dabei die Größe, wir wollen ein quadratisches Feld, dafür reicht eine Variable die Methode nur in die von Play einfügen
+    public void PlayButton()
+    {
+        //start(Wert)
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+
+     
+}
