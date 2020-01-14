346aa0161b6d4ec9e829ef5284a5727c6b17bdea
ParaMotor <nico13898@aol.com>
2020-01-14 22:26:34 +0100

    Vorversionen

---
+++ script/AstarHex.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Astar3coordsImportantMethods
+{
+	public int distance(int sourceX, int sourceY, int sourceZ, int objectX, int objectY, int objectZ)
+	{
+		
+		return (Math.Abs(sourceX - objectX) + Math.Abs(sourceY - objectY) + Math.Abs(sourceZ - objectZ)) / 2;
+
+	}
+	public List<AstarHex> neighbors(int sourceX, int sourceY, int sourceZ, int objectX, int objectY, int objectZ, int currentStep, int width, int height, Grid grid)
+    {
+		List<AstarHex> neighborsClass = new List<AstarHex>(); //0-x, 1-y, 2-z
+
+		UnityEngine.Debug.Log(sourceX + ", " + sourceY + ", " + sourceZ);
+		neighborsClass.Add(new AstarHex() { xCoordinate = sourceX, yCoordinate = (sourceY - 1), zCoordinate = (sourceZ - 1), currentSteps = currentStep + 1, leastStepsToGoal = distance(sourceX, sourceY - 1, sourceZ - 1, objectX, objectY, objectZ) });
+		if (neighborsClass[0].xCoordinate < 0 - (neighborsClass[0].yCoordinate / 2) || neighborsClass[0].xCoordinate > width - (neighborsClass[0].yCoordinate / 2) || neighborsClass[0].yCoordinate < 0 || neighborsClass[0].yCoordinate > height || neighborsClass[0].zCoordinate < 0 || neighborsClass[0].zCoordinate >= 13) { neighborsClass[0].betretbar = false; }
+		//UnityEngine.Debug.Log(neighborsClass[0].xCoordinate + ", " + neighborsClass[0].yCoordinate + ", " + neighborsClass[0].zCoordinate + ", " + neighborsClass[0].currentSteps + ", " + neighborsClass[0].leastStepsToGoal);
+		for (int i = 0; i < grid.HexList.Count; i++)
+		{
+
+			if (grid.HexList[i].xCoordinate == neighborsClass[0].xCoordinate && grid.HexList[i].yCoordinate == neighborsClass[0].yCoordinate && grid.HexList[i].zCoordinate == neighborsClass[0].zCoordinate)
+			{
+
+				if (grid.HexList[i].betretbar == false) { neighborsClass[0].betretbar = grid.HexList[i].betretbar; }
+				break;
+
+			}
+
+		}
+
+		neighborsClass.Add(new AstarHex() { xCoordinate = sourceX + 1, yCoordinate = sourceY - 1, zCoordinate = sourceZ, currentSteps = currentStep + 1, leastStepsToGoal = distance(sourceX + 1, sourceY - 1, sourceZ, objectX, objectY, objectZ) });
+		if (neighborsClass[1].xCoordinate < 0 - (neighborsClass[1].yCoordinate / 2) || neighborsClass[1].xCoordinate > width - (neighborsClass[1].yCoordinate / 2) || neighborsClass[1].yCoordinate < 0 || neighborsClass[1].yCoordinate > height || neighborsClass[1].zCoordinate < 0 || neighborsClass[1].zCoordinate >= 13) { neighborsClass[1].betretbar = false; }
+		//UnityEngine.Debug.Log(neighborsClass[1].xCoordinate + ", " + neighborsClass[1].yCoordinate + ", " + neighborsClass[1].zCoordinate + ", " + neighborsClass[1].currentSteps + ", " + neighborsClass[1].leastStepsToGoal);
+		for (int i = 0; i < grid.HexList.Count; i++)
+		{
+
+			if (grid.HexList[i].xCoordinate == neighborsClass[1].xCoordinate && grid.HexList[i].yCoordinate == neighborsClass[1].yCoordinate && grid.HexList[i].zCoordinate == neighborsClass[1].zCoordinate)
+			{
+
+				if (grid.HexList[i].betretbar == false) { neighborsClass[1].betretbar = grid.HexList[i].betretbar; }
+				break;
+
+			}
+
+		}
+
+		neighborsClass.Add(new AstarHex() { xCoordinate = sourceX + 1, yCoordinate = sourceY, zCoordinate = sourceZ + 1, currentSteps = currentStep + 1, leastStepsToGoal = distance(sourceX + 1, sourceY, sourceZ + 1, objectX, objectY, objectZ) });
+		if (neighborsClass[2].xCoordinate < 0 - (neighborsClass[2].yCoordinate / 2) || neighborsClass[2].xCoordinate > width - (neighborsClass[2].yCoordinate / 2) || neighborsClass[2].yCoordinate < 0 || neighborsClass[2].yCoordinate > height || neighborsClass[2].zCoordinate < 0 || neighborsClass[2].zCoordinate >= 13) { neighborsClass[2].betretbar = false; }
+		//UnityEngine.Debug.Log(neighborsClass[2].xCoordinate + ", " + neighborsClass[2].yCoordinate + ", " + neighborsClass[2].zCoordinate + ", " + neighborsClass[2].currentSteps + ", " + neighborsClass[2].leastStepsToGoal);
+		for (int i = 0; i < grid.HexList.Count; i++)
+		{
+
+			if (grid.HexList[i].xCoordinate == neighborsClass[2].xCoordinate && grid.HexList[i].yCoordinate == neighborsClass[2].yCoordinate && grid.HexList[i].zCoordinate == neighborsClass[2].zCoordinate)
+			{
+
+				if (grid.HexList[i].betretbar == false) { neighborsClass[2].betretbar = grid.HexList[i].betretbar; }
+				break;
+
+			}
+
+		}
+
+		neighborsClass.Add(new AstarHex() { xCoordinate = sourceX, yCoordinate = sourceY + 1, zCoordinate = sourceZ + 1, currentSteps = currentStep + 1, leastStepsToGoal = distance(sourceX, sourceY + 1, sourceZ + 1, objectX, objectY, objectZ) });
+		if (neighborsClass[3].xCoordinate < 0 - (neighborsClass[3].yCoordinate / 2) || neighborsClass[3].xCoordinate > width - (neighborsClass[3].yCoordinate / 2) || neighborsClass[3].yCoordinate < 0 || neighborsClass[3].yCoordinate > height || neighborsClass[3].zCoordinate < 0 || neighborsClass[3].zCoordinate >= 13) { neighborsClass[3].betretbar = false; }
+		//UnityEngine.Debug.Log(neighborsClass[3].xCoordinate + ", " + neighborsClass[3].yCoordinate + ", " + neighborsClass[3].zCoordinate + ", " + neighborsClass[3].currentSteps + ", " + neighborsClass[3].leastStepsToGoal);
+		for (int i = 0; i < grid.HexList.Count; i++)
+		{
+
+			if (grid.HexList[i].xCoordinate == neighborsClass[3].xCoordinate && grid.HexList[i].yCoordinate == neighborsClass[3].yCoordinate && grid.HexList[i].zCoordinate == neighborsClass[3].zCoordinate)
+			{
+
+				if (grid.HexList[i].betretbar == false) { neighborsClass[3].betretbar = grid.HexList[i].betretbar; }
+				break;
+
+			}
+
+		}
+
+		neighborsClass.Add(new AstarHex() { xCoordinate = sourceX - 1, yCoordinate = sourceY + 1, zCoordinate = sourceZ, currentSteps = currentStep + 1, leastStepsToGoal = distance(sourceX - 1, sourceY + 1, sourceZ, objectX, objectY, objectZ) });
+		if (neighborsClass[4].xCoordinate < 0 - (neighborsClass[4].yCoordinate / 2) || neighborsClass[4].xCoordinate > width - (neighborsClass[4].yCoordinate / 2) || neighborsClass[4].yCoordinate < 0 || neighborsClass[4].yCoordinate > height || neighborsClass[4].zCoordinate < 0 || neighborsClass[4].zCoordinate >= 13) { neighborsClass[4].betretbar = false; }
+		//UnityEngine.Debug.Log(neighborsClass[4].xCoordinate + ", " + neighborsClass[4].yCoordinate + ", " + neighborsClass[4].zCoordinate + ", " + neighborsClass[4].currentSteps + ", " + neighborsClass[4].leastStepsToGoal);
+		for (int i = 0; i < grid.HexList.Count; i++)
+		{
+
+			if (grid.HexList[i].xCoordinate == neighborsClass[4].xCoordinate && grid.HexList[i].yCoordinate == neighborsClass[4].yCoordinate && grid.HexList[i].zCoordinate == neighborsClass[4].zCoordinate)
+			{
+
+				if (grid.HexList[i].betretbar == false) { neighborsClass[4].betretbar = grid.HexList[i].betretbar; }
+				break;
+
+			}
+
+		}
+
+		neighborsClass.Add(new AstarHex() { xCoordinate = sourceX - 1, yCoordinate = sourceY, zCoordinate = sourceZ - 1, currentSteps = currentStep + 1, leastStepsToGoal = distance(sourceX - 1, sourceY, sourceZ - 1, objectX, objectY, objectZ) });
+		if (neighborsClass[5].xCoordinate < 0 - (neighborsClass[5].yCoordinate / 2) || neighborsClass[5].xCoordinate > width - (neighborsClass[5].yCoordinate / 2) || neighborsClass[5].yCoordinate < 0 || neighborsClass[5].yCoordinate > height || neighborsClass[5].zCoordinate < 0 || neighborsClass[5].zCoordinate >= 13) { neighborsClass[5].betretbar = false; }
+		//UnityEngine.Debug.Log(neighborsClass[5].xCoordinate + ", " + neighborsClass[5].yCoordinate + ", " + neighborsClass[5].zCoordinate + ", " + neighborsClass[5].currentSteps + ", " + neighborsClass[5].leastStepsToGoal);
+		for (int i = 0; i < grid.HexList.Count; i++)
+		{
+
+			if (grid.HexList[i].xCoordinate == neighborsClass[5].xCoordinate && grid.HexList[i].yCoordinate == neighborsClass[5].yCoordinate && grid.HexList[i].zCoordinate == neighborsClass[5].zCoordinate)
+			{
+
+				if (grid.HexList[i].betretbar == false) { neighborsClass[5].betretbar = grid.HexList[i].betretbar; }
+				break;
+
+			}
+
+		}
+
+		return neighborsClass;
+
+    }
+}
+
+public class Astar3coords
+{
+
+	Astar3coordsImportantMethods AstarIM = new Astar3coordsImportantMethods();
+
+	public void Astar(Hex objectT, Hex start, Grid grid)
+	{
+
+		//int maxX = grid.gridWidth;
+		//int maxY = grid.gridHeight;
+		int maxX = 10000;
+		int maxY = 10000;
+		int objectX = objectT.xCoordinate;
+		int objectY = objectT.yCoordinate;
+		int objectZ = objectT.zCoordinate;
+		int startX = start.xCoordinate;
+		int startY = start.yCoordinate;
+		int startZ = start.zCoordinate;
+		int zwischenX = -1;
+		int zwischenY = -1;
+		int zwischenZ = -1;
+		int counter = 0;
+
+		Boolean abort = false;
+		AstarHex current;
+		List<AstarHex> neighbors = new List<AstarHex>();
+		int[] neighborCost = new int[6];
+		int noWay = 0;
+		int oneNeighborCost = maxX * maxY;
+		int debugcounter = 0;
+
+		List<AstarHex> AstarHex = new List<AstarHex>();
+		HashSet<AstarHex> ClosedList = new HashSet<AstarHex>();
+
+		UnityEngine.Debug.Log("Ende: " + objectX + ", " + objectY + ", " + objectZ + ";");
+		AstarHex.Add(new AstarHex() { xCoordinate = startX, yCoordinate = startY, zCoordinate = startZ, currentSteps = 0, leastStepsToGoal = AstarIM.distance(startX, startY, startZ, objectX, objectY, objectZ)});
+		UnityEngine.Debug.Log("current Hex: " + AstarHex[0].xCoordinate + ", " + AstarHex[0].yCoordinate + ", " + AstarHex[0].zCoordinate + ";");
+		current = AstarHex[0];
+
+		while (AstarHex.Count > 0 && abort == false)
+		{
+			current = AstarHex[0];
+
+			for (int i = 0; i < AstarHex.Count(); i++)
+			{
+
+				if (AstarHex[i].leastStepsToGoal + AstarHex[i].currentSteps <= current.leastStepsToGoal + current.currentSteps && AstarHex[i].leastStepsToGoal < current.leastStepsToGoal)
+				{
+					current = AstarHex[i];
+				}
+
+			}
+			AstarHex.Remove(current);
+			ClosedList.Add(current);
+			UnityEngine.Debug.Log("current Hex: " + current.xCoordinate + ", " + current.yCoordinate + ", " + current.zCoordinate + ";");
+
+			if (current.xCoordinate == objectX && current.yCoordinate == objectY && current.zCoordinate == objectZ) { abort = true; } //Abbruch wenn gefunden
+
+			neighbors = AstarIM.neighbors(current.xCoordinate, current.yCoordinate, current.zCoordinate, objectX, objectY, objectZ, current.currentSteps, 10, 10, grid); //Richtige Width/Height noch nachliefern!
+
+			UnityEngine.Debug.Log(neighbors.Count);
+
+			for (int i = 0; i < neighbors.Count; i++)
+            {
+				for (int j = 0; j < grid.HexList.Count; j++)
+				{
+					if (grid.HexList[j].xCoordinate == neighbors[i].xCoordinate && grid.HexList[j].yCoordinate == neighbors[i].yCoordinate && grid.HexList[j].zCoordinate == neighbors[i].zCoordinate)
+                    {
+						grid.HexList[j].ChangeColor(2);
+						continue;
+					}
+						
+				}
+
+				//UnityEngine.Debug.Log(neighbors[i].xCoordinate + ", " + neighbors[i].yCoordinate + ", " + neighbors[i].zCoordinate);
+				if (neighbors[i].betretbar == false || ClosedList.Contains(neighbors[i]))
+                {
+
+					UnityEngine.Debug.Log("continue?");
+					continue;
+
+                }
+				if ((current.currentSteps + current.leastStepsToGoal) < (neighbors[i].currentSteps + neighbors[i].leastStepsToGoal) || AstarHex.Contains(neighbors[i]) == false)
+                {
+
+					neighbors[i].parent = current;
+					AstarHex.Add(neighbors[i]);
+
+                }
+
+            }
+
+		}
+				while (zwischenX != startX && zwischenY != startY && zwischenZ != startZ && counter < 12){
+
+					zwischenX = current.xCoordinate;
+					zwischenY = current.yCoordinate;
+					zwischenZ = current.zCoordinate;
+
+					for (int j = 0; j < grid.HexList.Count; j++)
+					{
+						if (grid.HexList[j].xCoordinate == current.xCoordinate && grid.HexList[j].yCoordinate == current.yCoordinate && grid.HexList[j].zCoordinate == current.zCoordinate)
+						{
+							grid.HexList[j].ChangeColor(3);
+							continue;
+						}
+
+					}
+
+					current = current.parent;
+
+					counter++;
+				}
+
+
+	}
+
+
+}
+
+
+public class AstarHex
+{
+	public int currentSteps;
+	public int leastStepsToGoal;
+	public int xCoordinate { get; set; }
+	public int yCoordinate { get; set; }
+	public int zCoordinate { get; set; }
+	public Boolean betretbar = true;
+	public AstarHex parent;
+
+
+	public Boolean getBetretbar()
+	{
+		return betretbar;
+	}
+
+
+}
---
+++ script/Voreinstellungen.cs
@@ -0,0 +1,268 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Voreinstellungen : MonoBehaviour
+{
+//Variablen
+    public Grid grid;
+
+    //für Gridgröße eintellen (also das Inputfield)
+    private int groesse;
+    bool istDezimal = true;
+    private int auswahl;
+
+    //für InputField
+    public string GridChange;
+    public GameObject inputField;
+
+    //Slider für Delay
+    public Slider slider;
+
+    //Funktionen für Start- und Zielfestlegung
+    public Hex Start;
+    public Hex Ziel;
+
+    //Bei PlayButton
+    public int geseheneFelder = 0;
+    //public GameObject Character;
+    public Transform SpawnCharacter;
+    public GameObject charClone;
+    public Hex before;
+    
+
+    //"Inventar"
+    public Transform SpawnRotationSchreibtisch;
+    public Transform SpawnRotationChair;
+    public Transform SpawnRotationSofa;
+    public GameObject BuecherregalObjekt;
+    public GameObject SchreibtischObjekt;
+    public GameObject SofaObjekt;
+    public GameObject ChairObjekt;
+
+    public List<Hex> HexListObject;
+//--------------------------------------------------------------------------------
+
+//Methoden für das UI
+
+    //wechsel zum Hauptmenü
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    /* - dient zur eingabe der Größe des Grids
+     * - prüft ob die Eingabe in das Inputfield nur aus Zahlen besteht
+     * - ertellt ein Grid mit der eingegebenen Größe
+     */
+    public void GridGroesse()
+    {
+        istDezimal = true;
+        GridChange = inputField.GetComponent<Text>().text;   //holt String aus InputField und schreibt ihn auf die Variable
+        //überprüft ob alle eingegebenen Zeichen Zahlen sind, weil Unity z.B. "." zulässt
+        foreach (char zeichen in GridChange)
+        {
+            if (!char.IsNumber(zeichen))
+            {
+                istDezimal = false;
+                Debug.Log("Bitte nur Zahlen eingeben");
+                break;
+            }
+        }
+        //Grid wird nur erstellt, wenn bei der Eingabe NUR Zahlen eingegeben wurden
+        if (istDezimal == true)
+        {           
+            groesse = int.Parse(GridChange);                    //groesse wird mit dem in Int gewandelten Wert von der Eingabe überschrieben
+            if(groesse > 25)
+            {
+                groesse = 25;
+                grid.Beginn(groesse);
+            }
+            else
+            {
+                grid.Beginn(groesse);                               //die methode Beginn() vom Script Grid wird aufgerufen und der parameter wird übergeben
+            }
+            
+        }
+
+    }
+
+    /*nach buttonclick wird das Start-Hex als "geclickt" gefärbt
+    * dann wird das geclickte Feld wird als "Start" markiert
+    * die Farbe des Feldes wird auf die "StartFarbe" gesetzt*/
+    public void StartpunktButton()
+    {
+        if (Start != null)
+            Start.ResetColor();
+        Start = grid.GetClicked();
+        Start.IsStart();
+    }
+
+    /*nach buttonclick wird das Ziel-Hex als "geclickt" gefärbt
+     * dann wird das geclickte Feld wird als "Ziel" markiert
+     * die Farbe des Feldes wird auf die "ZielFarbe" gesetzt*/
+    public void ZielpunktButton()
+    {
+        if (Ziel != null)
+            Ziel.ResetColor();
+        Ziel = grid.GetClicked();
+        Ziel.IsEnde();
+    }
+
+    /*hier wird der Algorithmus ausgewählt und die Auswahl in als Zahl gespeichert, 
+     * sodass beim "Play"-drücken der ausgewählte Algorithmus benutzt werden kann
+     */
+    public void AlgoAuswahl(int val)
+    {
+        if (val == 0)
+        {          
+            auswahl = 0;
+            Debug.Log("Tiefensuche");
+        }
+        if (val == 1)
+        {
+            auswahl = 1;
+            Debug.Log("Breitensuche");
+        }
+        if (val == 2)
+        {
+            auswahl = 2;
+            Debug.Log("A*");
+        }
+    }
+
+    /*Die Nummer des Ausgewählte Algorithmus wird abgefragt damit der entsprechende Algorithmus, 
+     * mit Start- und Ziel-Hex "geladen" werden kann.
+     * Der Algorithmus wird dann ausgeführt.
+     * der "Scenenwechsel" erfolgt durch Unity, mit dem ein-/ausblenden des jeweiligen Canvas, automatisch
+     */
+    public void PlayButton()
+    {
+        Astar3coords Astar = new Astar3coords();
+
+        switch (auswahl)
+        {
+            case 0:
+                if (Start != null && Ziel != null)
+                {
+                    //sorgt dafür das wenn der Algorithmus läuft, die Farben der Hexagons nicht verändert werden können
+                    foreach (Hex g in grid.HexList) { g.colorChangeOn = false; } 
+                   
+                    Debug.Log("Tiefensuche wird ausgeführt");
+                    grid.GetComponent<Tiefensuche>().enabled = true;
+                    grid.GetComponent<Tiefensuche>().Start = Start;
+                    grid.GetComponent<Tiefensuche>().Ende = Ziel;
+                    grid.GetComponent<Tiefensuche>().Anfang();
+                    //CharakterSpwan();
+                    break;
+                }
+                else
+                {
+                    Debug.Log("fehler bei Platzierung von Start- und Zielpunkt");
+                    break;
+                }
+            case 1:
+                if (Start != null && Ziel != null)
+                {
+                    foreach (Hex g in grid.HexList) { g.colorChangeOn = false; }
+                    Debug.Log("Breitensuche wird ausgeführt");
+                    grid.GetComponent<Breitensuche>().enabled = true;
+                    grid.GetComponent<Breitensuche>().Start = Start;
+                    grid.GetComponent<Breitensuche>().Ende = Ziel;
+                    grid.GetComponent<Breitensuche>().Anfang();
+                    //CharakterSpwan();
+                    break;
+                }
+                else
+                {
+                    Debug.Log("fehler bei Platzierung von Start- und Zielpunkt");
+                    break;
+                }
+            case 2:
+                Debug.Log("auswahl: A* ");
+                if (Start != null && Ziel != null)
+                {
+                    Astar.Astar(Ziel, Start, grid);
+                }
+                else
+                {
+                    Debug.Log("fehler bei Platzierung von Start- und Zielpunkt");
+                }
+                break;
+        }
+    }
+
+    //Inventar: Erzeugung der Hindernisse
+    public void SpawnRegal()
+    {
+        Instantiate(BuecherregalObjekt, Vector3.up, SpawnRotationSofa.rotation);
+    }
+    public void SpawnSchreibtisch()
+    {
+        Instantiate(SchreibtischObjekt, Vector3.up, SpawnRotationSchreibtisch.rotation);
+    }
+    public void SpawnSofa()
+    {
+        Instantiate(SofaObjekt, Vector3.up, SpawnRotationSofa.rotation);
+    }
+    public void SpawnChair()
+    {
+        Instantiate(ChairObjekt, Vector3.up, SpawnRotationChair.rotation);
+    }
+
+    //ändert das Delay mit dem der algorithmus durch läuft
+    public void ChangeDelay()
+    {
+        grid.searchDelay = slider.value;
+    }
+
+    //Methode für den Zurückbutton in "InGameUI"
+    public void HexagonsWiederFaerbbarMachen()
+    {
+        //beim Zurückgehen wird "colorChangeOn" auf true gesetzt, damit die felder wieder gefärbt werden können
+        foreach (Hex g in grid.HexList) { g.colorChangeOn = true; }
+    }
+
+    //ZurückButton wenn man "Ingame" ist, also nicht in den Einstellungen
+    public void ZurueckButton()
+    {
+        if (Start != null && Ziel != null)
+        {
+            Start.IsStart();
+            Ziel.IsEnde();
+        }
+        switch (auswahl)
+        {
+            case 0:
+                grid.GetComponent<Tiefensuche>().run = false;
+                grid.GetComponent<Tiefensuche>().enabled = false;
+                break;
+            case 1:
+                grid.GetComponent<Breitensuche>().run = false;
+                grid.GetComponent<Breitensuche>().enabled = false;
+                break;
+        }
+        //Destroy(charClone);
+    }
+    //Für charakter Erstellung und Bewegung
+    public void CharakterSpwan()
+    {
+        List<Hex> pfadRev = grid.GetComponent<Tiefensuche>().pathList;
+        Vector3 direction;
+        Hex before = pfadRev[pfadRev.Count - 1];
+        Instantiate(charClone, pfadRev[pfadRev.Count - 1].transform.position, SpawnCharacter.rotation);
+        pfadRev.RemoveAt(pfadRev.Count - 1);
+        for (int i = pfadRev.Count ; i > 0; i--)
+        {
+            direction = before.transform.position - pfadRev[pfadRev.Count - 1].transform.position;
+            for (int j = 4; j > 0; j--)
+            {
+                charClone.transform.position = charClone.transform.position + direction * 0.25f;
+            }
+            before = pfadRev[pfadRev.Count - 1];
+            pfadRev.RemoveAt(pfadRev.Count - 1);
+        }
+
+    }
+}
