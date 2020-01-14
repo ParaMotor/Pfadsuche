44e64244edc0561d8fa959bacb9290bcea420e85
ParaMotor <nico13898@aol.com>
2020-01-14 14:13:34 +0100

    Now with 3 Coordinates!

--- Hex.cs
+++ Hex.cs
@@ -1,19 +1,63 @@
-ï»¿using System;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hex : MonoBehaviour
 {
-    public Material[] farbe; //0=aktuell 1=blockiert 2=Entdeckt 3=pfad 4=start 5=ziel
+    public Material[] farbe; //0=aktuell 1=blockiert 2=Entdeckt 3=pfad 4=start 5=ziel 6=normal
     public int xCoordinate { get; set; }
     public int yCoordinate { get; set; }
+    public int zCoordinate { get; set; }
     public Boolean betretbar = true;
     public Boolean entdeckt = false;
+    public Boolean clicked = false;
     public Hex previous;
-    public int dijkstra_abstand = int.MaxValue;
+    public bool colorChangeOn = true;
+    //public int dijkstra_abstand = int.MaxValue;
     public List<Hex> nachbarn = new List<Hex>();
 
-    //Hex Koordinaten
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit Hitinfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out Hitinfo))
+            {
+                if (colorChangeOn)
+                {
+                    if (Hitinfo.collider == GetComponent<MeshCollider>())
+                    {
+                        clicked = true;
+                        ChangeColor(1);
+                    }
+                    else
+                    {
+                        clicked = false;
+                        ChangeColor(6);
+                    }
+                }
+            }
+        }
+        //Für Update der Kollision von Hindernissen
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.up, out hit))
+        {
+            betretbar = false;
+        }
+        else
+        {
+            betretbar = true;
+        }
+        if (!betretbar)
+        {
+            ChangeColor(1);
+        }
+    }
+       
+        //Hex Koordinaten
     public void SetX(int x)
     {
         xCoordinate = x;
@@ -23,21 +67,26 @@ public class Hex : MonoBehaviour
         yCoordinate = y;
     }
 
-    //Hex Funktionen
+    public void SetZ(int z)
+    {
+        zCoordinate = z;
+    }
+
+    //Hex Nachbarn
     public List<Hex> getNachbarn()
     {
         return nachbarn;
     }
+    public void addNachbar(Hex nachb)
+    {
+        if (nachb != null)
+            nachbarn.Add(nachb);
+    }
+
+    //Hex Betretbar
     public Boolean getBetretbar()
     {
-        return betretbar;        
-    }
-    public Hex getPrevious()
-    {
-        if (previous == null)
-            return null;
-        else
-            return previous;
+        return betretbar;
     }
     public void setBetretbar(Boolean betr)
     {
@@ -45,15 +94,22 @@ public class Hex : MonoBehaviour
         if (betr == false)
             ChangeColor(1);
     }
+
+
+    //Hex Vorherigen
+    public Hex getPrevious()
+    {
+        if (previous == null)
+            return null;
+        else
+            return previous;
+    }
     public void setPrevious(Hex prev)
     {
         previous = prev;
     }
-    public void addNachbar(Hex nachb)
-    {
-        if (nachb!=null)
-            nachbarn.Add(nachb);
-    }
+
+
     public Boolean GetEntdeckt()
     {
         return entdeckt;
@@ -64,14 +120,27 @@ public class Hex : MonoBehaviour
         if (wert == true)
             ChangeColor(2);
     }
+
     public void ChangeColor(int c)
     {
         Renderer rend;
         rend = GetComponentInParent<Renderer>();
-        rend.sharedMaterial = farbe[c];
+        //rend.sharedMaterial = farbe[c];
     }
-            
 
-    
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    public void ShowNeighbors()
+    {
+        foreach (Hex g in nachbarn)
+            g.ChangeColor(2);
+    }
+    public Transform getTransform()
+    {
+        return GetComponent<RectTransform>();
+    }
 
 }
